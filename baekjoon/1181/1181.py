d3b50074fc29768da10226206c8f6eee25aa095f
sangjin <toramx5@gmail.com>
2021-08-18 00:12:08 +0900

    0818 commit

--- baekjoon/1181/1181.py
+++ baekjoon/1181/1181.py
@@ -2,6 +2,11 @@ N = int(input())
 words = []
 for _ in range(N):
     words.append(input())
+# 중복제거를 위해 set으로 변환 후, 다시 list로 변환
+words = list(set(words))
+
+# 첫번째는 길이, 두번째는 알파벳 순서로 정렬
+words.sort(key=lambda x: [len(x), x])
 
 for word in words:
-    len(word)
+    print(word)
